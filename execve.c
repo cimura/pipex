9f5a485bd475d6ba7fc05bfad82c10d2f986797f
Shunsuke Shimura <sshimura@c2r8s2.42tokyo.jp>
2024-07-25 16:00:05 +0900

    execve

--- execve.c
+++ execve.c
@@ -1,13 +1,30 @@
-#include <unistd.h>
 #include <stdio.h>
-
-int main(void)
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+ 
+int main(int argc, char *argv[])
 {
-	char	*const args[] = {"ls", "-al", NULL};
-	char	*const envp[] = {NULL};
-
-	execve("/usr/bin/ls", args, envp);
-
-	perror("execve");
-	return (1);
+	pid_t pid;
+ 
+	pid = fork();
+	if (pid == -1)
+	{
+		perror("fork");
+		exit(EXIT_FAILURE);
+	}
+	else if (pid == 0)
+	{
+		printf("I am the child process.\n");
+		sleep(2);
+		exit(EXIT_SUCCESS);
+	}
+	else
+	{
+		printf("I am the parent process.\n");
+		// wait(NULL);
+		printf("Child process terminated after a 2s delay.\n");
+	}
+ 
+	return (EXIT_SUCCESS);
 }
